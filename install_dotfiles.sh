c9a29cec2c44b2944fb8ef07dd45971ca09d6a25
zetlen <zetlen@gmail.com>
2015-10-26 07:37:31 -0500

    Add vundle to install script

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -1,1 +1,4 @@
 for file in {.,}*; do [ -f "$file" ] && ln -s $(pwd)/"$file" ~/"$file"; done
+if [ ! -f ~/.vim/bundle/Vundle.vim ]; then
+  git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
