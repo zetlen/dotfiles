f9408a6ca3ce12f54725bb6ae43443498de09ba7
zetlen <zetlen@gmail.com>
2016-09-23 19:47:18 -0700

    fix sessions

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -1,4 +1,4 @@
-for file in {.,}*; do [ -f "$file" ] && ln -s $(pwd)/"$file" ~/"$file"; done
-if [ ! -f ~/.vim/bundle/Vundle.vim ]; then
-  git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+for file in {.,}*; do [ -f "$file" ] && ln -s $(pwd)/"$file" ~/"$file" 2> /dev/null; done
+if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
+  git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim;
 fi
