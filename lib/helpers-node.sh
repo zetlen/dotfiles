8ad0948a18247b44b6fc3f37add3c897a6686af8
zetlen <jzetlen@adobe.com>
2018-10-08 00:29:38 -0500

    npm vs yarn catcher

--- lib/helpers-node.sh
+++ lib/helpers-node.sh
@@ -1,4 +1,12 @@
-alias n=npm
+#!/bin/bash
+
+n() {
+  if [ -f "./yarn.lock" ]; then
+    printf "A yarn.lock file is present in this folder.\\nUse 'npm' to force using npm, or use 'y' for yarn instead.\\n"
+    return 1;
+  fi
+  npm "$@"
+}
 alias nr='npm run'
 alias nenv='printf "node $(node -v)\nnpm $(npm -v)\nyarn $(yarn --version)\n"'
 alias nreset='rm -rf ./node_modules && npm install'
