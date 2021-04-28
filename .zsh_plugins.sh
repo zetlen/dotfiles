bd9b25335414ab62cbdbd747ffde2bfa4330fa23
James Zetlen <jzetlen@adobe.com>
2021-04-28 15:17:50 -0500

    do not commit .zsh_plugins.sh

--- .zsh_plugins.sh
+++
@@ -1,7 +0,0 @@
-source /Users/jzetlen/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-romkatv-SLASH-powerlevel10k/powerlevel10k.zsh-theme
-source /Users/jzetlen/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-romkatv-SLASH-powerlevel10k/powerlevel9k.zsh-theme
-fpath+=( /Users/jzetlen/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-romkatv-SLASH-powerlevel10k )
-source /Users/jzetlen/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-mafredri-SLASH-zsh-async/async.plugin.zsh
-fpath+=( /Users/jzetlen/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-mafredri-SLASH-zsh-async )
-source /Users/jzetlen/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-allanjamesvestal-SLASH-fast-zsh-nvm/fast-loading-nvm.zsh
-fpath+=( /Users/jzetlen/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-allanjamesvestal-SLASH-fast-zsh-nvm )
