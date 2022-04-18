ad7524cc6b85ce979ee32c73bbb03ecedf6eb46a
zetlen <zetlen@gmail.com>
2022-04-18 11:04:54 -0500

    switch home lan tld

--- lib/helpers-local.sh
+++ lib/helpers-local.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 ssh-ha() {
-  ssh -tt -p 22222 root@homeassistant.home.arpa $1
+  ssh -tt -p 22222 root@homeassistant.amberly $1
 }
 ssh-ha-container() {
 	ssh-ha 'docker exec -it $(docker ps -qf "name='"$1"'") /bin/bash'
