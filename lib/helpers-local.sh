6056dd9427c576ea53c1b9d2b09e60e73866a421
= <=>
2022-07-04 16:37:00 -0500

    fix sshha

--- lib/helpers-local.sh
+++ lib/helpers-local.sh
@@ -3,5 +3,5 @@ ssh_ha() {
   ssh -tt -p 22222 root@homeassistant.amberly $1
 }
 ssh_ha_container() {
-	ssh-ha 'docker exec -it $(docker ps -qf "name='"$1"'") /bin/bash'
+	ssh_ha 'docker exec -it $(docker ps -qf "name='"$1"'") /bin/bash'
 }
