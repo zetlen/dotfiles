e05cfd9245e88b4f2aaac4260ecd1b8574951f93
zetlen <zetlen@gmail.com>
2022-08-13 12:50:46 -0500

    fix add_os_rc which has been broken

--- lib/common.sh
+++ lib/common.sh
@@ -115,7 +115,7 @@ get_os_dotfile_path() {
 }
 
 add_os_rc() {
-  THE_SHELL="$2"
+  THE_SHELL="$1"
   # fail silently if it doesn't exist
   . "$(get_os_dotfile_path)/.${THE_SHELL}rc" 2>/dev/null || true
 }
