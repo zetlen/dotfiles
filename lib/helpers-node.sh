84f5d66762cb887312eb95ab6771706f8bff5236
zetlen <zetlen@gmail.com>
2022-09-10 13:23:52 -0500

    better nreset

--- lib/helpers-node.sh
+++ lib/helpers-node.sh
@@ -18,6 +18,9 @@ nr() {
 }
 
 alias nenv='printf "node $(node -v)\nnpm $(npm -v)\nyarn $(yarn --version)\n"'
-alias nreset='rm -rf ./node_modules && npm install'
+alias delete_node_modules='find . -name "node_modules" -type d -prune -exec rm -rf '{}' +'
+alias clean_repo='npm run --if-present clean; git clean -dfX'
+alias yarn_or_npm_install='[ -f yarn.lock ] && yarn || npm install'
+alias nreset='set -x; delete_node_modules; clean_repo; yarn_or_npm_install; set +x'
 alias y=yarn
 alias yr='yarn run'
