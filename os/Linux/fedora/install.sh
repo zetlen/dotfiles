c84e1860c610b7599e96a7d74891776b4fc8dae0
zetlen <zetlen@gmail.com>
2022-12-22 17:52:59 -0600

    correct fedora logs

--- os/Linux/fedora/install.sh
+++ os/Linux/fedora/install.sh
@@ -17,7 +17,7 @@ __pkg_is_available() {
 	dnf info "$1" &> /dev/null
 }
 
-flog_success "Fedora Linux detected. Using apt package manager."
+flog_success "Fedora Linux detected. Using dnf package manager."
 __INSTALLABLE="$(__pkg_get_installable ${__TO_INSTALL_FEDORA[@]})"
 confirm_cmd "sudo dnf install -y $__INSTALLABLE"
 
