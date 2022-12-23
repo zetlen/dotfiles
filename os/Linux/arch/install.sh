d407520ea809cd393ad036a1eef82c4b21985396
zetlen <zetlen@gmail.com>
2022-12-23 06:56:49 -0600

    arch

---
+++ os/Linux/arch/install.sh
@@ -0,0 +1,31 @@
+__TO_INSTALL=(
+	zsh
+	git
+	curl
+	lsof
+	wget
+	exa
+	jq
+	vim-common
+	vim-enhanced
+	ripgrep
+	fzf
+	gnupg2
+	bat
+)
+
+__pkg_is_installed() {
+	pacman -Qi "$1" &> /dev/null
+}
+
+__pkg_is_available() {
+	local to_check="$1"
+	local apt_search_exp="$(printf '^%s$' "$to_check")"
+	test -n "$(pacman -Ss "$apt_search_exp")"
+}
+
+__pkg_install_all() {
+	pacman-key --init && pacman-key --populate && sudo pacman -Sy --noconfirm archlinux-keyring
+	__INSTALLABLE="$(__pkg_get_installable ${__TO_INSTALL[@]})"
+	test -n "$__INSTALLABLE" && confirm_cmd "sudo pacman -Sy --noconfirm $__INSTALLABLE"
+}
