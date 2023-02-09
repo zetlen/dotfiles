2f4541202b29ef45ce2265340c98bd6f530ddf3f
zetlen <zetlen@gmail.com>
2023-02-09 15:59:58 -0600

    even better node aliases

--- lib/common/helpers-node.sh
+++ lib/common/helpers-node.sh
@@ -1,5 +1,4 @@
 __set_node_aliases () {
-	 # emulate -L zsh
 
 	alias y='yarn '
 	alias yr='yarn run '
@@ -16,17 +15,19 @@ __set_node_aliases () {
 			alias n="echo $__yarn_notice && yarn "
 			alias nr="echo $__yarn_notice && yarn run "
 			alias ni="echo $__yarn_notice && yarn add "
+			alias __n_install_all="yarn"
 		fi
 	else
 			alias n='npm '
 			alias nr='npm run '
 			alias ni='npm install '
+			alias __n_install_all="npm install"
 	fi
 
-	alias clean_project='npm run --if-present clean && in_repo && git clean -dfX || true'
+	alias clean_project='npm run --if-present clean && in_repo && git wash || true'
 	alias delete_node_modules='find . -name "node_modules" -type d -prune -exec rm -rf '{}' +'
 	alias nenv='printf "node $(node -v)\nnpm $(npm -v)\nyarn $(yarn --version)\n"'
-	alias nreset='set -x; clean_project; delete_node_modules; ni; set +x'
+	alias nreset='set -x; clean_project; delete_node_modules; __n_install_all; set +x'
 }
 
 add_cd_hook __set_node_aliases
