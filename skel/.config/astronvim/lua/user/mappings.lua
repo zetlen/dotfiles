9ba4d9df1837f193e8880d8a782432393b41ea47
zetlen <zetlen@gmail.com>
2023-05-01 13:56:39 -0500

    astronvim

---
+++ skel/.config/astronvim/lua/user/mappings.lua
@@ -0,0 +1,12 @@
+return {
+    n = {
+      ["<leader>c"] = {
+        function()
+          local bufs = vim.fn.getbufinfo { buflisted = true }
+          require("astronvim.utils.buffer").close(0)
+          if require("astronvim.utils").is_available "alpha-nvim" and not bufs[2] then require("alpha").start(true) end
+        end,
+        desc = "Close buffer",
+      },
+    },
+}
