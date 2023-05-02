4db6dfce49d19ec917d5f93adf160f178abc8a99
zetlen <zetlen@gmail.com>
2023-05-02 02:32:04 +0000

    update nvim script

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -108,7 +108,7 @@ __zdi_step3() {
 	if i_have nvim; then
 		mkdir -p ~/.config
 		git clone --depth 1 https://github.com/AstroNvim/AstroNvim ~/.config/nvim
-		nvim --headless -c 'autocmd User LazyDone quitall'
+		nvim --headless -c 'quitall'
 	else
 		flog_warn "Neovim is not installed."
 	fi
