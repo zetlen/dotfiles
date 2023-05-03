050606e088376d535257acda30152f087c6a4eb7
zetlen <zetlen@gmail.com>
2023-05-03 07:49:05 -0500

    alpine

---
+++ os/Linux/alpine/install.sh
@@ -0,0 +1,35 @@
+__TO_INSTALL=(
+	alpine-sdk
+	curl
+	exa
+	fzf
+	git
+	gnupg
+	jq
+	lsof
+	openssh
+	openssl
+	ripgrep
+	tk
+	unzip
+	vim
+	wget
+	xz
+	zlib
+	zsh
+)
+
+__pkg_is_installed() {
+	apk info "$1" &>/dev/null
+}
+
+__pkg_is_available() {
+	test -n "$(apk search -e "$1")"
+}
+
+__pkg_install_all() {
+	apk update
+	apk upgrade
+	__INSTALLABLE="$(__pkg_get_installable ${__TO_INSTALL[@]})"
+	test -n "$__INSTALLABLE" && confirm_cmd "apk add $__INSTALLABLE"
+}
