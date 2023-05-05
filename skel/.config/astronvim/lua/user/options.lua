df18f4617385c247a745492171d3b16c5415f355
zetlen <zetlen@gmail.com>
2023-05-05 19:13:14 +0000

    neovim options

---
+++ skel/.config/astronvim/lua/user/options.lua
@@ -0,0 +1,9 @@
+return {
+  opt = {
+    list = true, -- show whitespace characters
+    listchars = { tab = "│→", extends = "⟩", precedes = "⟨", trail = "·", nbsp = "␣" },
+    showbreak = "↪ ",
+    swapfile = false,
+		relativenumber = false
+  },
+}
