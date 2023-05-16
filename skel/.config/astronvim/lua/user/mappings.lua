c00b24c5802e49268c31aaae6c45b881da3affff
zetlen <zetlen@gmail.com>
2023-05-16 10:26:23 -0500

    better nvim ff mapping

--- skel/.config/astronvim/lua/user/mappings.lua
+++ skel/.config/astronvim/lua/user/mappings.lua
@@ -1,12 +1,22 @@
 return {
-    n = {
-      ["<leader>c"] = {
-        function()
-          local bufs = vim.fn.getbufinfo { buflisted = true }
-          require("astronvim.utils.buffer").close(0)
-          if require("astronvim.utils").is_available "alpha-nvim" and not bufs[2] then require("alpha").start(true) end
-        end,
-        desc = "Close buffer",
-      },
-    },
+	n = {
+		["<leader>c"] = {
+			function()
+				local bufs = vim.fn.getbufinfo({ buflisted = true })
+				require("astronvim.utils.buffer").close(0)
+				if require("astronvim.utils").is_available("alpha-nvim") and not bufs[2] then
+					require("alpha").start(true)
+				end
+			end,
+			desc = "Close buffer",
+		},
+		["<leader>ff"] = {
+			function()
+				require("telescope.builtin").find_files({
+					find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/**", "--glob", "!**/node_modules/**" },
+				})
+			end,
+			desc = "Find file",
+		},
+	},
 }
