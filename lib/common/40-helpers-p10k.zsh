f69dbd5a9696e9b65bb38b76090b0839997e5f48
zetlen <zetlen@gmail.com>
2023-05-24 20:17:51 +0000

    p10k helper fns

--- lib/common/40-helpers-p10k.zsh
+++ lib/common/40-helpers-p10k.zsh
@@ -1,5 +1,13 @@
 #!/bin/zsh
 
-_p10k_show_versions() {
+_p10k_toggle_versions() {
 	p10k display '*/asdf'=hide,show
+}
+
+_p10k_hide_versions() {
+	p10k display '*/asdf'=hide
+}
+
+_p10k_show_versions() {
+	p10k display '*/asdf'=show
 }
