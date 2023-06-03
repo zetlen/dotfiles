88c1ce2edf3b4da2d7b1d14ee627338b16ef4751
zetlen <zetlen@gmail.com>
2023-06-03 21:20:17 +0000

    add vim-surround

--- skel/.config/astronvim/lua/user/plugins.lua
+++ skel/.config/astronvim/lua/user/plugins.lua
@@ -63,6 +63,16 @@ return {
 		config = require("plugins.configs.notify"),
 	},
 	{
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+        require("nvim-surround").setup({
+            -- Configuration here, or leave empty to use defaults
+        })
+    end,
+	},
+	{
 		"kevinhui/vim-docker-tools",
 		cmd = {
 			"DockerToolsToggle",
