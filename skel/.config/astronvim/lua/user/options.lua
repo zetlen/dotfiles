fed16cbd600bf18e21191e306f2f26ee182174c1
zetlen <zetlen@gmail.com>
2023-06-03 21:20:05 +0000

    astronvim switch to resession

--- skel/.config/astronvim/lua/user/options.lua
+++ skel/.config/astronvim/lua/user/options.lua
@@ -1,9 +1,12 @@
 return {
-  opt = {
-    list = true, -- show whitespace characters
-    listchars = { tab = "│→", extends = "⟩", precedes = "⟨", trail = "·", nbsp = "␣" },
-    showbreak = "↪ ",
-    swapfile = false,
-		relativenumber = false
-  },
+	opt = {
+		list = true, -- show whitespace characters
+		listchars = { tab = "│→", extends = "⟩", precedes = "⟨", trail = "·", nbsp = "␣" },
+		showbreak = "↪ ",
+		swapfile = false,
+		relativenumber = false,
+	},
+	g = {
+		resession_enabled = true,
+	},
 }
