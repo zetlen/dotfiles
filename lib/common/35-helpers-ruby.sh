f7a21f75908310e680bfde22526daa8fe94e6f66
zetlen <zetlen@gmail.com>
2023-08-24 17:29:58 -0500

    ruby puma helper

--- lib/common/35-helpers-ruby.sh
+++ lib/common/35-helpers-ruby.sh
@@ -2,3 +2,7 @@ alias bx="bundle exec "
 alias bi="bundle install "
 alias ba="bundle add "
 alias br="bin/rails "
+
+puma-sites() {
+	(printf 'HOST\tPROJECT\n'; for app in $HOME/.puma-dev/*.*; do printf 'https://%s.test\t%s\n' $(basename $app) $(readlink -f $app | xargs basename); done) | column -t -c '\t' | sed "1s/.*/$(tput bold)&$(tput sgr0)/"
+}
