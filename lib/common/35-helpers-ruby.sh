580d54fcabcb1c6de6c4b1e13318a84d6d2ff8eb
zetlen <zetlen@gmail.com>
2023-09-29 10:18:44 -0500

    add ruby diagnostic report viewer

--- lib/common/35-helpers-ruby.sh
+++ lib/common/35-helpers-ruby.sh
@@ -1,8 +1,11 @@
 alias bx="bundle exec " 
 alias bi="bundle install "
-alias ba="bundle add "
 alias br="bin/rails "
 
 puma-sites() {
 	(printf 'HOST\tPROJECT\n'; for app in $HOME/.puma-dev/*.*; do printf 'https://%s.test\t%s\n' $(basename $app) $(readlink -f $app | xargs basename); done) | column -t -c '\t' | sed "1s/.*/$(tput bold)&$(tput sgr0)/"
+}
+
+ruby_get_report() {
+	ls -t ~/Library/Logs/DiagnosticReports/ruby* | head -n 1 | xargs -n 1 realpath | xargs open
 }
