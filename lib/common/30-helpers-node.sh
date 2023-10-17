8f0d67b2739f852ccf6dc880bd9fe48374a2c900
zetlen <zetlen@gmail.com>
2023-10-17 17:06:25 -0400

    pnpm aliases

--- lib/common/30-helpers-node.sh
+++ lib/common/30-helpers-node.sh
@@ -3,25 +3,32 @@ __set_node_aliases() {
 	alias n='npm '
 	alias nr='npm run '
 	alias ni='npm install '
-	alias __n_install_all="npm install"
+	alias __n_install_all='npm install'
 	alias y='yarn '
 	alias yr='yarn run '
+	alias p='pnpm '
+	alias pr='pnpm run '
 
 	local yarnlock="$(find_up yarn.lock)"
 	local packagelock="$(find_up package-lock.json)"
+	local pnpmlock="$(find_up pnpm-lock.yaml)"
 
 	__yarn_notice="yarn.lock detected, aliasing npm to yarn"
-
 	if [ -n "$yarnlock" ]; then
-		if [ -n "$packagelock" ]; then
-			__yarn_notice="both package-lock.json and yarn.lock are present, defaulting to npm"
-		else
-			alias n="echo $__yarn_notice && yarn "
-			alias nr="echo $__yarn_notice && yarn run "
-			alias ni="echo $__yarn_notice && yarn add "
-			alias __n_install_all="yarn"
-		fi
+		alias n="echo $__yarn_notice && yarn "
+		alias nr="echo $__yarn_notice && yarn run "
+		alias ni="echo $__yarn_notice && yarn add "
+		alias __n_install_all="yarn"
 	fi
+
+	__pnpm_notice="pnpm-lock.yaml detected, aliasing npm to pnpm"
+	if [ -n "$pnpmlock" ]; then
+		alias n="echo $__pnpm_notice && pnpm "
+		alias nr="echo $__pnpm_notice && pnpm run "
+		alias ni="echo $__pnpm_notice && pnpm add "
+		alias __n_install_all="pnpm install"
+	fi
+
 
 	alias clean_project='npm run --if-present clean && in_repo && git wash || true'
 	alias delete_node_modules='find . -name "node_modules" -type d -prune -exec rm -rf '{}' +'
