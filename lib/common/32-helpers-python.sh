a8d2a183e41be5dca304147187f2915b3a679329
zetlen <zetlen@gmail.com>
2024-03-02 10:58:17 -0600

    better python

--- lib/common/32-helpers-python.sh
+++ lib/common/32-helpers-python.sh
@@ -1,8 +1,18 @@
-alias p_vinit="python -m venv venv && source ./venv/bin/activate"
-alias p_vactivate="source ./venv/bin/activate"
-alias p_vfreeze="pip freeze > requirements.txt"
-alias p_vinstall="pip install -r requirements.txt"
+export PIP_REQUIRE_VIRTUALENV=true
+
+pip() {
+  python -m pip "$@"
+}
 
 p_vadd() {
-  pip install --upgrade $1 && pip freeze > requirements.txt
+  pip install --upgrade $@ && pip freeze > requirements.txt
 }
+
+gpip() {
+  PIP_REQUIRE_VIRTUALENV="" python -m pip "$@"
+}
+
+alias p_vinit="python -m venv venv && source ./venv/bin/activate"
+alias p_vactivate="source ./venv/bin/activate"
+alias p_vfreeze="python -m pip freeze > requirements.txt"
+alias p_vinstall="python -m pip install -r requirements.txt"
