0bdc3367520abad1a2f2121b3661fcb3c383fa08
zetlen <zetlen@gmail.com>
2024-03-16 16:25:19 -0500

    docker helpers

---
+++ lib/common/25-helpers-docker.sh
@@ -0,0 +1,9 @@
+alias docker-compose="docker compose "
+alias dco="docker compose "
+alias dcou="dco --wait --wait-timeout 10 -d "
+alias dcouf="docker compose pull && dcou --force-recreate "
+
+
+dsh() {
+  docker exec -it $1 ${2:-bash}
+}
