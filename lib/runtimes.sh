1f82eb46514fb1eb22a714a7e1d873454432a2a4
zetlen <zetlen@gmail.com>
2024-06-11 13:35:33 -0500

    explicit hook-env

--- lib/runtimes.sh
+++ lib/runtimes.sh
@@ -3,7 +3,7 @@ setup_runtimes() {
   local mise_bin_loc="${HOME}/.local/bin/mise"
   [ -x "$mise_bin_loc" ] && \
     eval "$($mise_bin_loc activate zsh)" && \
-    eval "$($mise_bin_loc hook-env)"
+    eval "$($mise_bin_loc hook-env -s zsh)"
 }
 
 setup_runtimes
