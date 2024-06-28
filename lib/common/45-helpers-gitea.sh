fbb6a4bc0eab3723e3e00eae0548ed01057b1fbc
zetlen <zetlen@gmail.com>
2024-06-28 09:49:23 -0500

    removing some personal stuff

--- lib/common/45-helpers-gitea.sh
+++ lib/common/45-helpers-gitea.sh
@@ -1,10 +1,11 @@
 teaset() {
   local repo="$(basename $PWD)"
-  local remote="${1:-got.in.the-z-machine.com}"
-  local user="${2:-zetlen}"
+  local remote="${1:-got}"
+  local me=$(whoami)
+  local user="${2:-${me}}"
   git remote -v | grep -F "$remote" && echo "Already there!" && return 0
 
   tea repo create --name "$repo" && \
-    git remote add origin "git@${remote}:${2-zetlen}/$repo" && \
+    git remote add origin "git@${remote}:${2-${me}}/$repo" && \
     git push --all
 }
