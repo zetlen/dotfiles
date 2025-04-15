ce580609eced5d5f01b977528733817438cd16c5
zetlen <zetlen@gmail.com>
2025-04-15 09:54:42 -0500

    improved node aliases

--- lib/common/30-helpers-node.sh
+++ lib/common/30-helpers-node.sh
@@ -1,9 +1,5 @@
 __set_node_aliases() {
 
-	alias n='npm '
-	alias nr='npm run '
-	alias ni='npm install '
-	alias __n_install_all='npm install'
 	alias y='yarn '
 	alias yr='yarn run '
 	alias p='pnpm '
@@ -13,31 +9,28 @@ __set_node_aliases() {
 	alias prf='pnpm run --filter '
 	alias prfip='pnpm run --filter --if-present '
 
-	local yarnlock="$(find_up yarn.lock)"
-	local packagelock="$(find_up package-lock.json)"
-	local pnpmlock="$(find_up pnpm-lock.yaml)"
+  _pm='npm'
+  __notice=''
 
-	__yarn_notice="yarn.lock detected, aliasing npm to yarn"
-	if [ -n "$yarnlock" ]; then
-		alias n="echo $__yarn_notice && yarn "
-		alias nr="echo $__yarn_notice && yarn run "
-		alias ni="echo $__yarn_notice && yarn add "
-		alias __n_install_all="yarn"
-	fi
+  alias n='echo -n $__notice && $_pm '
+  alias nr="n run"
 
-	__pnpm_notice="pnpm-lock.yaml detected, aliasing npm to pnpm"
-	if [ -n "$pnpmlock" ]; then
-		alias n="echo $__pnpm_notice && pnpm "
-		alias nr="echo $__pnpm_notice && pnpm run "
-		alias ni="echo $__pnpm_notice && pnpm add "
-		alias __n_install_all="pnpm install"
-	fi
+  ni() {
+    [[ "$1" == "" ]] && $_pm install || $_pm add $@
+  }
 
+	if [ -n "$(find_up yarn.lock)" ]; then
+    __notice="yarn.lock detected, aliasing npm to yarn\n"
+    _pm='yarn'
+  elif [ -n "$(find_up pnpm-lock.yaml)" ]; then
+    __notice="pnpm-lock.yaml detected, aliasing npm to pnpm\n"
+    _pm='pnpm'
+  fi
 
-	alias clean_project='npm run --if-present clean && in_repo && git wash || true'
+	alias clean_project="$_pm run --if-present clean && in_repo && git wash || true"
 	alias delete_node_modules='find . -name "node_modules" -type d -prune -exec rm -rf '{}' +'
-	alias nenv='printf "node $(node -v)\nnpm $(npm -v)\nyarn $(yarn --version)\n"'
-	alias nreset='set -x; clean_project; delete_node_modules; __n_install_all; set +x'
+  alias nenv='printf "node %s\n%s %s\s" "$(node -v)" $_pm "$($_pm --version)"' 
+	alias nreset='clean_project; delete_node_modules; ni;'
 }
 
 add_cd_hook __set_node_aliases
