2c4454539bd96b52027cdff1d9346c2d775cf287
zetlen <zetlen@gmail.com>
2025-12-05 08:08:52 -0600

    crucial updates

--- lib/common/45-helpers-gitea.sh
+++ lib/common/45-helpers-gitea.sh
@@ -1,11 +1,11 @@
 teaset() {
     local repo="$(basename $PWD)"
-    local remote="${1:-got}"
+    local remote="${1:-https://got.colonpipe.org}"
     local me=$(whoami)
     local user="${2:-${me}}"
     git remote -v | grep -F "$remote" && echo "Already there!" && return 0
 
-    tea repo create --name "$repo" &&
+    tea repos create --name "$repo" &&
         git remote add origin "git@${remote}:${2-${me}}/$repo" &&
         git push --all
 }
